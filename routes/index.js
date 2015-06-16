2b738dca96b9adc1a23ea846e916b396ceeb9bd9
Carlos <carlossala92@gmail.com>
2015-06-16 20:33:03 +0200

    Añadido Creditos

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 var quizController = require("../controllers/quiz_controller");
 
-/* GET home page. */
+// Carga la pagina de inicio
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Carga la pagina de creditos
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
+//Controladores para preguntas y respuestas
 router.get("/quizes/question", quizController.question);
 router.get("/quizes/answer", quizController.answer);
 
